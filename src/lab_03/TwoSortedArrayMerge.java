ee960525d37062abfe047e6c3f1f82b243b5dd05
unknown <Saigon Manager@SGN-PC049-1.vn.defide.co.jp>
2022-05-26 18:36:41 +0700

    Lab 03

--- src/lab_03/TwoSortedArrayMerge.java
+++ src/lab_03/TwoSortedArrayMerge.java
@@ -4,7 +4,7 @@ public class TwoSortedArrayMerge {
 
     public static void main(String[] args) {
         int[] sortedArrayNum1 = {1, 12, 16, 28, 34};
-        int[] sortedArrayNum2 = {1, 13, 16, 99, 27};
+        int[] sortedArrayNum2 = {1, 13, 16, 27, 99};
         int[] newSortedArrayNum = new int[sortedArrayNum1.length + sortedArrayNum2.length];
         int temp = 0;
         boolean isMinValue = false;
@@ -24,9 +24,11 @@ public class TwoSortedArrayMerge {
                     int j = 1;
                     //thay dổi gia tri cua tung element
                     for (int k = 0 + indexNewArray; k < newSortedArrayNum.length; k++) {
+                        if(j != newSortedArrayNum.length) {
                             temp = newSortedArrayNum[newSortedArrayNum.length - 1 - j];
                             newSortedArrayNum[newSortedArrayNum.length - j] = temp;
                             j++;
+                        }
                     }
                     newSortedArrayNum[indexNewArray] = sortedArrayNum2[indexArrayNum2];
                     break;
